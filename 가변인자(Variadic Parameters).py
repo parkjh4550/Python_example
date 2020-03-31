33736aa5b93a76d96b89c9a9504be0fbdba620c0
parkjh4550 <37648792+parkjh4550@users.noreply.github.com>
2020-04-01 02:08:28 +0900

    Add files via upload

--- 가변인자(Variadic Parameters).py
+++ 가변인자(Variadic Parameters).py
@@ -8,6 +8,7 @@ def sum_all(*args):
     result = 0
     for i in args:
         result += i
+    print('type of *args : ', type(args))
     return result
 
 def sum_all_with_name(**kwargs):
@@ -18,12 +19,27 @@ def sum_all_with_name(**kwargs):
     for k, v in kwargs.items():
         names.append(k)
         result+=v
-
+    print('type of **kwargs : ', type(kwargs))
     return result, names
 
+def sum_all_both(*args, **kwargs):
+    # *args : list
+    # **kwargs : dictionary
+    result = 0
+    names = []
+
+    for i in args:
+        result += i
+
+    for k, v in kwargs.items():
+        names.append(k)
+        result += v
+
+    return result, names
 
 if __name__ == '__main__':
     numbers = [1,2,3,4]
     print(sum_numbers(numbers[0], numbers[1]))
     print(sum_all(numbers[0], numbers[1], numbers[2], numbers[3]))
     print(sum_all_with_name(one=numbers[0], two=numbers[1], three=numbers[2], four=numbers[3]))
+    print(sum_all_both(numbers[0], numbers[1], three=numbers[2], four=numbers[3]))
